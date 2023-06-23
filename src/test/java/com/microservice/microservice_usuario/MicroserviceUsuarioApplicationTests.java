26515fca48baf901b908b97fa74079d2c9b243dd
Daniel Ramos <dramosg@ucentral.edu.co>
2023-06-23 13:01:47 -0500

    add structure

---
+++ src/test/java/com/microservice/microservice_usuario/MicroserviceUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservice_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
